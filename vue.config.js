410d50441b04f7b299b4ee79d4b412be40aee8ed
user <a0903131602@gmail.com>
2024-10-13 15:54:34 +0800

    dist 修改01

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // eslint-disable-next-line key-spacing
+  publicPath:'/Cli-demo/dist/'
 })
