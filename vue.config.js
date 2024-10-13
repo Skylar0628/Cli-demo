8f9cf8a3a5456468c19ebe6e325334d37ef030a4
user <a0903131602@gmail.com>
2024-10-13 16:11:50 +0800

    套用bootstrap套件 修改dist 路徑

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // eslint-disable-next-line key-spacing
-  publicPath:'/Cli-demo/'
+  publicPath:'/Cli-demo/dist/'
 })
