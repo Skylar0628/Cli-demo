b23a50bc1f9b296ca15197cb88d109d4a06f32e4
user <a0903131602@gmail.com>
2024-10-20 00:07:34 +0800

    套用全域的千分號

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Loading from 'vue3-loading-overlay'
+import { currency } from './methods/filter'
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 
 const app = createApp(App)
-
+app.config.globalProperties.$filters = {
+  currency
+}
 app.component('Loading', Loading)
 app.use(VueAxios, axios)
 
